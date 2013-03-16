98aec16aba81b8fa5553fe9e823ef52899b711c4
Dr-Zhi <yl.zhi@hotmail.com>
2013-03-16 20:20:11 +0800

    add solutions for polulating next right pionters problems

---
+++ OnlineJudge/PolulatingNextRightPointersInEachNodeIISolution.h
@@ -0,0 +1,14 @@
+//
+//  PolulatingNextRightPointersInEachNodeIISolution.h
+//  OnlineJudge
+//
+//  Created by Yanling Zhi on 3/16/13.
+//  Copyright (c) 2013 Dr. Zhi Inc. All rights reserved.
+//
+
+#ifndef OnlineJudge_PolulatingNextRightPointersInEachNodeIISolution_h
+#define OnlineJudge_PolulatingNextRightPointersInEachNodeIISolution_h
+
+
+
+#endif
---
+++ OnlineJudge/PolulatingNextRightPointersInEachNodeSolution.h
@@ -0,0 +1,14 @@
+//
+//  PopulatingNextRightPointersinEachNodeSolution.h
+//  OnlineJudge
+//
+//  Created by Yanling Zhi on 3/16/13.
+//  Copyright (c) 2013 Dr. Zhi Inc. All rights reserved.
+//
+
+#ifndef OnlineJudge_PopulatingNextRightPointersinEachNodeSolution_h
+#define OnlineJudge_PopulatingNextRightPointersinEachNodeSolution_h
+
+
+
+#endif
