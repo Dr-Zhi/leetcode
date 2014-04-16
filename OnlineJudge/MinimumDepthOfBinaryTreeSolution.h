dd5bb4d4ba0ca0e182edea295029c30d661c9ef2
Yanling Zhi <i@Dr-Zhi.com>
2014-04-16 21:31:01 +0800

    improve one solution

--- OnlineJudge/MinimumDepthOfBinaryTreeSolution.h
+++ OnlineJudge/MinimumDepthOfBinaryTreeSolution.h
@@ -21,7 +21,7 @@ struct TreeNode {
 };
 */
 
-/** http://leetcode.com/onlinejudge#question_111
+/** http://oj.leetcode.com/problems/minimum-depth-of-binary-tree/
  * Given a binary tree, find its minimum depth. The minimum depth is the
  * number of nodes along the shortest path from the root node down to the 
  * nearest leaf node.
@@ -29,15 +29,15 @@ struct TreeNode {
 class MinimumDepthOfBinaryTreeSolution {
 public:
     int minDepth(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (!root)
+        if (root == NULL) {
             return 0;
-        if (root->left == NULL && root->right == NULL) // leaf node
+        }
+        if (root->left == NULL && root->right == NULL) {
             return 1;
-        int left = root->left ? minDepth(root->left) : INT_MAX;
-        int right = root->right ? minDepth(root->right) : INT_MAX;
-        return min(left, right) + 1;
+        }
+        int depthLeft = root->left ? minDepth(root->left) : INT_MAX;
+        int depthRight = root->right ? minDepth(root->right) : INT_MAX;
+        return min(depthLeft, depthRight) + 1;
     }
 };
 
